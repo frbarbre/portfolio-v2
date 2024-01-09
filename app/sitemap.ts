c87352bfc1b05eaadb8e25890228db1fbf11c1ab
frbarbre <fr.barbre@gmail.com>
2024-01-09 14:30:57 +0100

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -26,5 +26,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'yearly',
       priority: 0.7,
     },
+    {
+      url: 'https://frederikbarbre.dk/works/archive',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
   ];
 }
