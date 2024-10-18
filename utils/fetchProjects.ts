2b4e82753d791de72a6556e04dfd96dec6e70652
frbarbre <fr.barbre@gmail.com>
2024-10-18 15:27:27 +0200

    setup caching

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -2,8 +2,7 @@ import { Project } from "@/types";
 
 export async function fetchProjects() {
   const data = await fetch(
-    "https://wp.frederikbarbre.dk/wp-json/wp/v2/posts?per_page=100&catagories=5",
-    { cache: "no-store" }
+    "https://wp.frederikbarbre.dk/wp-json/wp/v2/posts?per_page=100&catagories=5"
   );
   const projects = (await data.json()) as Project[];
   return projects;
